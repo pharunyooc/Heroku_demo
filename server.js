9a7a81aeb33db81d107f2bd294290fec00cde379
PJ <Pharunyooc@gmail.com>
2022-03-25 04:36:47 +0700

    init

---
+++ server.js
@@ -0,0 +1,13 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT||8080;
+
+
+app.get('/', (req, res)=> {
+   res.json({result: "ok" , data:[1,2,3,4,5]});
+})
+
+app.listen(PORT, () =>
+    console.log(`Server is running on ${PORT}`)
+)
